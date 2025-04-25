41634cfe46b148e1b469b663f892335eda298825
JL <jloeshelle@alarm.com>
2025-04-25 08:35:22 -0400

    Initial scaffold of app

---
+++ test/BasicCSharpApp.Tests/CalculatorTests.cs
@@ -0,0 +1,86 @@
+﻿using BasicCSharpApp.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicCSharpApp.Tests;
+
+[TestClass]
+public sealed class CalculatorTests
+{
+    private Calculator _calculator = null!;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _calculator = new Calculator();
+    }
+
+    [TestMethod]
+    public void Add_TwoNumbers_ReturnsCorrectSum()
+    {
+        // Arrange
+        double a = 5;
+        double b = 7;
+
+        // Act
+        double result = _calculator.Add(a, b);
+
+        // Assert
+        Assert.AreEqual(12, result);
+    }
+
+    [TestMethod]
+    public void Subtract_TwoNumbers_ReturnsCorrectDifference()
+    {
+        // Arrange
+        double a = 10;
+        double b = 3;
+
+        // Act
+        double result = _calculator.Subtract(a, b);
+
+        // Assert
+        Assert.AreEqual(7, result);
+    }
+
+    [TestMethod]
+    public void Multiply_TwoNumbers_ReturnsCorrectProduct()
+    {
+        // Arrange
+        double a = 4;
+        double b = 5;
+
+        // Act
+        double result = _calculator.Multiply(a, b);
+
+        // Assert
+        Assert.AreEqual(20, result);
+    }
+
+    [TestMethod]
+    public void Divide_TwoNumbers_ReturnsCorrectQuotient()
+    {
+        // Arrange
+        double a = 20;
+        double b = 4;
+
+        // Act
+        double result = _calculator.Divide(a, b);
+
+        // Assert
+        Assert.AreEqual(5, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(System.DivideByZeroException))]
+    public void Divide_ByZero_ThrowsDivideByZeroException()
+    {
+        // Arrange
+        double a = 10;
+        double b = 0;
+
+        // Act
+        _calculator.Divide(a, b);
+
+        // Assert is handled by ExpectedException attribute
+    }
+}
